f3b882b4b02a103aacb537cba23a7cb24ad067f5
jrodcab519 <jdrodriguezcabrero@hotmail.com>
2024-06-06 21:10:55 +0200

    Versión inicial

---
+++ src/Ejercicio2.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        List<Integer> listaEdades = new ArrayList<>();
+        System.out.println("Introduce una edad: (negativo para salir)");
+        int edad = s.nextInt();
+        double sumaEdad = 0;
+        int contador = 0;
+        int bajoMedia = 0;
+        int sobreMedia = 0;
+        while(edad >= 0){
+            sumaEdad += edad;
+            contador++;
+            listaEdades.add(edad);
+            edad = s.nextInt();
+            s.nextLine();
+        }
+
+        double media = sumaEdad / contador;
+
+        List<Integer> CopiaLista = List.copyOf(listaEdades);
+        Iterator<Integer> iterador = listaEdades.iterator();
+        Iterator<Integer> iterador2 = CopiaLista.iterator();
+
+        while (iterador.hasNext()){
+            if(iterador.next() < media){
+                bajoMedia++;
+            }
+        }
+
+        while (iterador2.hasNext()){
+            if(iterador2.next() > media){
+                sobreMedia++;
+            }
+        }
+
+        System.out.println("La edad media es: " +  media);
+        System.out.println("Hay " +  bajoMedia + " personas por debajo de la media");
+        System.out.println("Hay " +  sobreMedia + " personas por encima de la media");
+    }
+}
